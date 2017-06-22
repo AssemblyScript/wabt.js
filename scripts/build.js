3ad3c6064c4463e55d4c89a94feebad64fd0fadd
dcodeIO <dcode@dcode.io>
2017-06-22 12:23:28 +0200

    Separated CommonJS shim a bit

--- scripts/build.js
+++ scripts/build.js
@@ -4,10 +4,11 @@ var fs = require("fs");
 
 // Reuse pre-compiled JavaScript library for now
 var source = fs.readFileSync(__dirname + "/../wabt/demo/libwabt.js").toString()
-  .trim();
+  .trim()
+  .replace(/;$/, "");
 
 // Append a CommonJS shim that doesn't conflict with the global 'wabt' var
-source += ";if(\"undefined\"!=typeof module&&module&&module.exports)module.exports=wabt;";
+source += ";\n\nif(\"undefined\"!=typeof module&&module&&module.exports)module.exports=wabt;";
 
 // And save it back
 fs.writeFileSync(__dirname + "/../index.js", source, "utf8");
