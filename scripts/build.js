6f399ef9b99ac3c533adef65ff74b0aa37022736
dcodeIO <dcode@dcode.io>
2017-06-28 12:09:02 +0200

    Updated to reuse new upstream CommonJS/AMD loader support

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,10 @@
-// Note that this makes quite a few assumptions that might break at any point.
-
 var fs = require("fs");
 
 // Reuse pre-compiled JavaScript library for now
-var source = fs.readFileSync(__dirname + "/../wabt/demo/libwabt.js").toString()
-  .trim()
-  .replace(/;$/, "");
-
-// Append an UMD shim that doesn't conflict with the global 'wabt' var
-source += "\n\n" + [
-  'if("undefined"!=typeof module&&module&&module.exports)module.exports=wabt;',
-  'else if("function"==typeof define&&define.amd)define(function(){return wabt});'
-].join("");
+var source = fs.readFileSync(__dirname + "/../wabt/demo/libwabt.js")
+  .toString()
+  .replace(/(?:\r?\n)+/g, "\n")
+  .trim();
 
 // And save it back
 fs.writeFileSync(__dirname + "/../index.js", source, "utf8");
